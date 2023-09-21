f250707d19bfe66e3a29ce6fa32827c2a957e3c8
kevino29 <varax297@gmail.com>
2023-09-21 10:08:00 -0300

    updated grid size

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import Grid from './js/Grid.js';
 
+const size = 10, bombs = 15;
+
 window.addEventListener('load', () => {
     let game = document.querySelector('#game');
     let resetButton = document.querySelector('#resetButton');
     
-    let grid = new Grid(game,20,20,50);
+    let grid = new Grid(game,size,size,bombs);
     grid.create();
 
     resetButton.addEventListener('click', () => {grid.reset()} );
